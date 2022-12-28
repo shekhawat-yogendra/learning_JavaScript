93ab7dfc2eaf2a8ec499ddb9626c7c43405067a4
shekhawat-yogendra <76551612+shekhawat-yogendra@users.noreply.github.com>
2022-12-28 12:20:08 +0530

    added array file
    via file upload.

---
+++ array.js
@@ -0,0 +1,12 @@
+let class12_marks = [45,87,98,55,63, "Absent"]
+console.log(class12_marks)
+console.log(class12_marks.length)
+console.log(class12_marks[5])
+console.log(class12_marks[6])
+
+let name = "ram"
+console.log(name)
+name = "manish"
+console.log(name)
+class12_marks[5] = 34
+console.log(class12_marks[5])
